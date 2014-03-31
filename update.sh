7ca4ae66336a356a14235f0fbb013b2c38758903
Jack Kelly <jack-list@xlk.org.uk>
2014-03-31 11:49:24 +0100

    renaming UKPD to UK-DALE

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp ~/workspace/schemas/nilm_metadata/examples/ukpd_dataset.yaml ukpd_nilm_metadata.yaml
+cp ~/workspace/schemas/nilm_metadata/examples/UK-DALE_dataset.yaml UK-DALE_nilm_metadata.yaml
 
-~/workspace/schemas/nilm_metadata/scripts/process.py ukpd_nilm_metadata.yaml -o ukpd_nilm_metadata_concatenated.yaml
+~/workspace/schemas/nilm_metadata/scripts/process.py UK-DALE_nilm_metadata.yaml -o UK-DALE_nilm_metadata_concatenated.yaml
 
 echo "Done"
